e2b9bdc5cf8f793eba9ca738fb03d92defba129e
Lucas Pereira Santos <lucas.psantos64@gmail.com>
2026-01-14 18:46:43 -0300

    feat: implementacao da funcao adicionar tarefa

--- src/gerenciador.py
+++ src/gerenciador.py
@@ -9,3 +9,15 @@ class GerenciadorTarefas:
     def __init__(self):
         self.tarefas = []
         self._next_id = 1
+
+    def adicionar_tarefa(self, titulo, descricao, prioridade=Prioridade.MEDIA):
+        tarefa = {
+            "id": self._next_id,
+            "titulo": titulo,
+            "descricao": descricao,
+            "prioridade": prioridade.value,
+            "concluida": False
+        }
+        self.tarefas.append(tarefa)
+        self._next_id += 1
+        return tarefa
